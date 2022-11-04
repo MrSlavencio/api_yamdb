69af813ef85ad2da9c30dd6027a17438808f9a73
DannyDies <bachuslovevine@gmail.com>
2022-11-04 15:25:52 +0300

    fix model enum, email, etc

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,21 +1,28 @@
+from enum import Enum
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+class RoleChoice(Enum):
+    ADMIN = "admin"
+    MODERATOR = "moderator"
+    USER = "user"
+
+    @classmethod
+    def choices(cls):
+        return tuple((c.name, c.value) for c in cls)
+
+
 class User(AbstractUser):
-
-    class Role(models.IntegerChoices):
-        ADMIN = 1, _("Администратор")
-        MODERATOR = 2, _("Модератор")
-        USER = 3, _("Пользователь")
-
-    role = models.PositiveSmallIntegerField(
-        choices=Role.choices,
-        default=Role.USER,
+    email = models.EmailField(_('email address'), unique=True,)
+    role = models.TextField(
+        choices=RoleChoice.choices(),
+        default=RoleChoice.USER,
     )
-
     bio = models.TextField(
-        'Биография',
+        _('Биография'),
         blank=True
     )
+    REQUIRED_FIELDS = ["email", ]
